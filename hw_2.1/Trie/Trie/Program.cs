8874369e6fca8d22eb91bc3baeb7462181a97e06
ShakirovSamat <samatx-r@mail.ru>
2023-03-16 01:29:41 +0300

    added solution

---
+++ hw_2.1/Trie/Trie/Program.cs
@@ -0,0 +1,72 @@
+﻿using Trees.Trie;
+
+class Program
+{
+    static void Main()
+    {
+        Trie trie = new Trie();
+        string? command;
+        bool processing = true;
+        Console.WriteLine("Trie program\n");
+        Console.WriteLine("Commands:" +
+            "\n1 - Add new string" +
+            "\n2 - Remove string" +
+            "\n3 - get size" +
+            "\n4 - how many words starts with prefix" +
+            "\n5 - Is word in trie\n6 - close the program\n");
+
+        while(processing)
+        {
+            Console.Write("Enter command: ");
+            command = Console.ReadLine();
+            string? word;
+            switch (command)
+            {
+                case "1":
+                    Console.Write("Enter word: ");
+                    word = Console.ReadLine();
+                    trie.Add(word);
+                    break;
+
+                case "2":
+                    Console.Write("Enter word: ");
+                    word = Console.ReadLine();
+                    trie.Remove(word);
+                    break;
+
+                case "3":
+                    Console.WriteLine($"Size is {trie.GetSize()}");
+                    break;
+
+                case "4":
+                    Console.Write("Enter prefix: ");
+                    word = Console.ReadLine();
+                    Console.WriteLine($"{trie.HowManyStartsWithPrefix(word)} words starts with {word}");
+                    break;
+
+                case "5":
+                    Console.Write("Wnter word: ");
+                    word = Console.ReadLine();
+                    if (trie.Contains(word))
+                    {
+                        Console.WriteLine($"{word} is in trie");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{word} is NOT in trie");
+                    }
+                    break;
+
+                case "6":
+                    processing = false;
+                    break;
+
+                default:
+                    Console.WriteLine("Worng input");
+                    break;
+            }
+            Console.WriteLine();
+        }
+    }
+      
+}
---
+++ hw_2.1/Trie/Trie/Trie.cs
@@ -0,0 +1,150 @@
+﻿namespace Trees.Trie
+{
+    // Trie data structure realization
+    public class Trie
+    {
+        private TrieNode head;
+        private int size;
+
+        private class TrieNode
+        {
+            public char value;
+            public int branches;
+            public bool isTerminated;
+            public Dictionary<char, TrieNode> children;
+
+            public TrieNode(char value)
+            {
+                this.value = value;
+                children = new Dictionary<char, TrieNode>();
+                branches = 1;
+            }
+            public TrieNode()
+            {
+                children = new Dictionary<char, TrieNode>();
+                branches = 1;
+            }
+        }
+        public Trie()
+        {
+            head = new TrieNode();
+        }
+
+        //Adds new word to the trie
+        public bool Add(string word)
+        {
+            if (word == null || word == "")
+            {
+                Console.WriteLine("Wrong input");
+                return false;
+            }
+
+            if (Contains(word))
+            {
+                return false;
+            }
+
+            TrieNode currentTrieNode = head;
+            foreach(char sign in word)
+            {
+                if (currentTrieNode.children.ContainsKey(sign))
+                {
+                    currentTrieNode = currentTrieNode.children[sign];
+                    currentTrieNode.branches++;
+                }
+                else
+                {
+                    currentTrieNode.children.Add(sign, new TrieNode(sign));
+                    currentTrieNode = currentTrieNode.children[sign];
+                }
+            }
+
+            currentTrieNode.isTerminated = true;
+            ++size;
+            return true;
+        }
+
+        //Searchs the word in the trie
+        public bool Contains(string word)
+        {
+            if (word == null || word == "")
+            {
+                Console.WriteLine("Wrong input");
+                return false;
+            }
+            TrieNode currentTieNode = head;
+            foreach(char sign in word)
+            {
+                if (!currentTieNode.children.ContainsKey(sign))
+                {
+                    return false;
+                }
+                currentTieNode = currentTieNode.children[sign];
+            }
+                   
+            return currentTieNode.isTerminated;
+        }
+
+        //Deletes the word in the trie
+        public bool Remove(string word)
+        {
+            if (word == null || word == "")
+            {
+                Console.WriteLine("Wrong input");
+                return false;
+            }
+
+            if (!Contains(word))
+            {
+                return false;
+            }
+
+            --size;
+
+            TrieNode currentTrieNode = head.children[word[0]];
+            for (int i= 1; i < word.Length; ++i)
+            {
+                currentTrieNode.branches--;
+                if (currentTrieNode.children[word[i]].branches == 1)
+                {
+                    currentTrieNode.children.Remove(word[i]);
+                    return true;
+                }
+                currentTrieNode = currentTrieNode.children[word[i]];
+                
+            }
+            return true;
+        }
+
+        // Finds amount of words staring with the prefix
+        public int HowManyStartsWithPrefix(string prefix)
+        {
+            if (prefix == null || prefix == "")
+            {
+                Console.WriteLine("Wrong input");
+                return 0;
+            }
+
+            TrieNode currentTrieNode = head;
+            foreach(char sign in prefix)
+            {
+                if (currentTrieNode.children.ContainsKey(sign))
+                {
+                    currentTrieNode = currentTrieNode.children[sign];
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            return currentTrieNode.branches;
+        }
+
+        // returns amount of words in the trie
+        public int GetSize()
+        {
+            return size;
+        }
+
+    }
+}
