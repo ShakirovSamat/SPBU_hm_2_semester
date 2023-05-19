014625730e54cc8511095594c21a7a2e6c94cfc6
ShakirovSamat <samatx-r@mail.ru>
2023-05-19 15:25:05 +0300

    added second task

---
+++ task_2/task_2/PriorityQueue.cs
@@ -0,0 +1,19 @@
+﻿namespace program
+{
+    public class PriorityQueue<T> where T : IComparable<T>
+    {
+        BinaryHeap<T> binaryHeap = new BinaryHeap<T>();
+
+        public bool Empty { get { return binaryHeap.heapSize == 0; } }
+
+        public void Enqueue(T value, int priority)
+        {
+            binaryHeap.Add(value, priority);
+        }
+
+        public T Dequeue()
+        {
+            return binaryHeap.getMax();
+        }
+    }
+}
